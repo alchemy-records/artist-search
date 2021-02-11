372757e0da05dd000e2429600029d33fc3af063f
Perry Sittser <sittserp@gmail.com>
2021-02-11 11:22:04 -0800

    file structure, rfcp

---
+++ src/components/artist/Artist.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Artist(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+Artist.propTypes = {
+
+};
+
+export default Artist;
+
---
+++ src/components/release/ReleaseList.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function ReleaseList(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+ReleaseList.propTypes = {
+
+};
+
+export default ReleaseList;
+
