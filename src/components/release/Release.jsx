068cd779ff43d2e056fe67fc17b3ac37b0dd07f2
BRYANA <BRYANAKITCHEN@GMAIL.COM>
2021-02-11 18:39:02 -0500

    build out release presentational component

--- src/components/release/Release.jsx
+++ src/components/release/Release.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Release(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+function Release({ id, title, date }) {
+  return (
+    <div>
+      <h3>{title}</h3>
+      <p>{date}</p>
+    </div>
+  );
 }
 
 Release.propTypes = {
-
+  title: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired
 };
 
 export default Release;
