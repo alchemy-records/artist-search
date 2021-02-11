b9e92f5a08eb300f38237694bd4abbfb6b0916dc
Jena Boehm <boehmjena@gmail.com>
2021-02-11 13:16:29 -0800

    TDD from loading state to rendering artists -- RED

--- src/containers/HomeView.jsx
+++ src/containers/HomeView.jsx
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import { useArtists } from '../state/artistHooks';
+import ArtistList from '../components/artist/ArtistList';
 
 
 function HomeView(props) {
-  const [loading, setLoading] = useState();
+  const { loading, artists } = useArtists();
 
-  return (
-    <div>
-           Hello World - music search 
-    </div>
-  );
+  if (loading) return 'loading'
+  return <ArtistList artists={artists} />
 }
 
 HomeView.propTypes = {
--- src/containers/HomeView.test.jsx
+++ src/containers/HomeView.test.jsx
@@ -3,10 +3,12 @@ import { render, screen, waitFor } from '@testing-library/react';
 import HomeView from './HomeView';
 
 describe('App component', () => {
-  it('renders App', () => {
+  it('renders App', async() => {
     render(<HomeView />);
 
     screen.getByText('loading');
+
+    const listOfArtists = await screen.findByTestId('artists');
   });
 });
   
